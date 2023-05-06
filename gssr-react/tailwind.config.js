594dc018b169585eac838ffcdb5ac9292b832a45
thomasyonaha-mccoy <83908974+goat6@users.noreply.github.com>
2023-05-06 16:37:12 +0100

    added extra scale option 0.6

--- gssr-react/tailwind.config.js
+++ gssr-react/tailwind.config.js
@@ -5,9 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      scale: {
+        '60': '0.6',
+      }
+    },
   },
   plugins: [],
-  
+  darkMode:'class',
 }
 
