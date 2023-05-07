3c84fdb8a9d473d439a4966e74cfdedbacbc5fb1
thomasyonaha-mccoy <83908974+goat6@users.noreply.github.com>
2023-05-07 08:18:48 +0100

    added credits to bottom of the page

--- gssr-react/src/MainPage.tsx
+++ gssr-react/src/MainPage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Form from "./Form";
 import Button from "./Button";
 import ThemeButton from "./ThemeButton";
+import GitHubMark from "./assets/GitHubMark.svg";
 
 const MainPage = () => {
   return (
@@ -16,6 +17,33 @@ const MainPage = () => {
           <Button name="Join room" />
           <Button name="Create room" />
         </div>
+      </div>
+      <div className="w-full flex justify-center items-center h-20 fixed bottom-0 left-0 gap-3 font-mono">
+        <a
+          className="hover:rotate-180 inline-flex items-center duration-200"
+          href="https://github.com/RGCSERG/gssr"
+        >
+          <img
+            className="mx-auto object-center object-scale-down h-9 dark:invert"
+            src={GitHubMark}
+          ></img>
+        </a>
+        <p className="text-xl dark:text-white">
+          © 2023{" "}
+          <a
+            className="text-blue-500 hover:text-blue-600 underline dark:text-blue-400 dark:hover:text-blue-500"
+            href="https://github.com/RGCSERG"
+          >
+            RGCSERG
+          </a>{" "}
+          &{" "}
+          <a
+            className="text-blue-500 hover:text-blue-600 underline dark:text-blue-400 dark:hover:text-blue-500"
+            href="https://github.com/goat6"
+          >
+            goat6
+          </a>
+        </p>
       </div>
       <ThemeButton />
     </>
