b074a213e08cb4e3b4145e7a6daf9fabe38c3613
thomasyonaha-mccoy <83908974+goat6@users.noreply.github.com>
2023-05-08 11:50:54 +0100

    minor optimisations

--- gssr-react/src/Button.tsx
+++ gssr-react/src/Button.tsx
@@ -5,8 +5,9 @@ interface Props {
 const Button = ({ name }: Props) => {
   return (
     <button
-      className="text-shadow h-16 w-1/6 rounded-full text-2xl shadow-lg outline 
-    outline-4 outline-black hover:bg-gray-200 dark:text-white dark:outline-white dark:hover:bg-gray-800"
+      className="text-shadow h-16 w-1/6 select-none rounded-full text-2xl shadow-lg 
+    outline outline-4 outline-black hover:bg-gray-200 dark:text-white dark:outline-white 
+    dark:hover:bg-gray-800"
     >
       {name}
     </button>
---
+++ gssr-react/src/Credits.tsx
@@ -0,0 +1,29 @@
+import { BsGithub } from "react-icons/bs";
+
+const Credits = () => {
+  return (
+    <div
+      className="text-shadow fixed bottom-0 left-0 flex h-20 w-full select-none items-center 
+    justify-center gap-3 font-mono"
+    >
+      <a
+        className="inline-flex items-center duration-200 hover:scale-110 hover:animate-pulse"
+        href="https://github.com/RGCSERG/gssr"
+      >
+        <BsGithub className=" object-center dark:invert" size={35}></BsGithub>
+      </a>
+      <p className="text-xl dark:text-white">
+        © 2023{" "}
+        <a className="credit-text" href="https://github.com/RGCSERG">
+          RGCSERG
+        </a>{" "}
+        &{" "}
+        <a className="credit-text" href="https://github.com/goat6">
+          goat6
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default Credits;
