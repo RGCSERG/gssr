ac807d54185f51548c6fe6d19468940767490088
thomasyonaha-mccoy <83908974+goat6@users.noreply.github.com>
2023-05-08 11:50:50 +0100

    reformated

--- gssr-react/src/MainPage.tsx
+++ gssr-react/src/MainPage.tsx
@@ -1,7 +1,6 @@
 import Form from "./Form";
-import Button from "./Button";
 import ThemeButton from "./ThemeButton";
-import { BsGithub } from "react-icons/bs";
+import Credits from "./Credits";
 
 const MainPage = () => {
   return (
@@ -10,31 +9,9 @@ const MainPage = () => {
         <p className="text-shadow text-5xl font-bold motion-safe:animate-bounce dark:text-white">
           Welcome to gssr.
         </p>
-        <Form placehold="Enter a Username" name="username" />
-        <Form placehold="Enter a Room name" name="roomName" />
-        <div className="flex w-full items-center justify-center gap-5">
-          <Button name="Join room" />
-          <Button name="Create room" />
-        </div>
+        <Form />
       </div>
-      <div className="text-shadow fixed bottom-0 left-0 flex h-20 w-full select-none items-center justify-center gap-3 font-mono">
-        <a
-          className="inline-flex items-center duration-200 hover:scale-110 hover:animate-pulse"
-          href="https://github.com/RGCSERG/gssr"
-        >
-          <BsGithub className=" object-center dark:invert" size={35}></BsGithub>
-        </a>
-        <p className="text-xl dark:text-white">
-          © 2023{" "}
-          <a className="credit-text" href="https://github.com/RGCSERG">
-            RGCSERG
-          </a>{" "}
-          &{" "}
-          <a className="credit-text" href="https://github.com/goat6">
-            goat6
-          </a>
-        </p>
-      </div>
+      <Credits />
       <ThemeButton />
     </>
   );
