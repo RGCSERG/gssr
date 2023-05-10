a84640cfcdb87f158245e038fd293c53c6299b5c
thomasyonaha-mccoy <83908974+goat6@users.noreply.github.com>
2023-05-10 13:25:05 +0100

    started work on form inputs

--- gssr-react/src/Button.tsx
+++ gssr-react/src/Button.tsx
@@ -1,13 +1,15 @@
 interface Props {
   name: string;
+  onClick: () => void;
 }
 
-const Button = ({ name }: Props) => {
+const Button = ({ name, onClick }: Props) => {
   return (
     <button
       className="text-shadow h-16 w-1/6 select-none rounded-full text-2xl shadow-lg 
     outline outline-4 outline-black hover:bg-gray-200 dark:text-white dark:outline-white 
     dark:hover:bg-gray-800"
+      onClick={onClick}
     >
       {name}
     </button>
--- gssr-react/src/Form.tsx
+++ gssr-react/src/Form.tsx
@@ -1,14 +1,20 @@
 import InputField from "./InputField";
 import Button from "./Button";
+import { useState } from "react";
 
 const Form = () => {
+  const [formValues, setFormValues] = useState({
+    username: "",
+    roomName: "",
+  });
+
   return (
     <>
       <InputField placehold="Enter a Username" name="username" />
       <InputField placehold="Enter a Room name" name="roomName" />
       <div className="flex w-full items-center justify-center gap-6">
-        <Button name="Join room" />
-        <Button name="Create room" />
+        <Button onClick={() => console.log(formValues)} name="Join room" />
+        <Button onClick={() => console.log(formValues)} name="Create room" />
       </div>
     </>
   );
