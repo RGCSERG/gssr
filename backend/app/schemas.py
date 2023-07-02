64049a8a50f93ac193c67d5b0efb999c8bdb4468
RGCSERG <sergentrufus@gmail.com>
2023-07-02 22:02:38 +0100

    got react working

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, Union
+from datetime import datetime
 
 
 class Post(BaseModel):
---
+++ backend/react-app/src/components/Messages.tsx
@@ -0,0 +1,12 @@
+
+// PascalCasing
+function Message () {
+    // JSX: JavaScript XML
+    const name = 'Roo';
+    if (name)
+        return <h1>Hello {name}</h1>
+    return <h1>Hello Guest</h1>;
+};
+
+
+export default Message;
