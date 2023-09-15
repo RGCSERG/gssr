98c70b75a0378f0c44f8e391317917e0abfebbb5
RGCSERG <sergentrufus@gmail.com>
2023-09-15 11:47:04 +0100

    bad ui for now nad fixed double message (bodge)

--- client/gssr_frontend_remaster/src/pages/Chat.tsx
+++ client/gssr_frontend_remaster/src/pages/Chat.tsx
@@ -2,8 +2,18 @@ import React, { useEffect, useState } from "react";
 import { Socket } from "socket.io-client";
 import ScrollToBottom from "react-scroll-to-bottom";
 import { useParams } from "react-router-dom";
+import {
+  Container,
+  Row,
+  Col,
+  Form,
+  Button,
+  Card,
+  Alert,
+} from "react-bootstrap";
 
 interface Message {
+  id: number;
   room: string;
   author: string;
   message: string;
@@ -19,10 +29,15 @@ const Chat = ({ socket, user }: Props) => {
   const { room } = useParams();
   const [currentMessage, setCurrentMessage] = useState("");
   const [messageList, setMessageList] = useState<Message[]>([]);
+  function getRandomInt(min: number, max: number): number {
+    // Use Math.floor() to round down to the nearest integer
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 
   const sendMessage = async () => {
     if (currentMessage.trim() !== "" && room !== undefined) {
       const messageData: Message = {
+        id: getRandomInt(1000000000000, 9999999999999),
         room: room,
         author: user,
         message: currentMessage,
@@ -33,7 +48,20 @@ const Chat = ({ socket, user }: Props) => {
       };
 
       await socket.emit("send_message", messageData);
-      setMessageList((list) => [...list, messageData]);
+      setMessageList((list) => {
+        // Check if the message with the same ID exists in the list
+        const isMessageAlreadyInList = list.some(
+          (message) => message.id === messageData.id
+        );
+
+        // If the message with the same ID doesn't exist, add it to the list
+        if (!isMessageAlreadyInList) {
+          return [...list, messageData];
+        }
+
+        // If the message already exists, return the original list without any changes
+        return list;
+      });
       setCurrentMessage("");
     }
   };
@@ -47,49 +75,81 @@ const Chat = ({ socket, user }: Props) => {
 
   useEffect(() => {
     socket.on("receive_message", (data: Message) => {
-      setMessageList((list) => [...list, data]);
+      setMessageList((list) => {
+        // Check if the message with the same ID exists in the list
+        const isMessageAlreadyInList = list.some(
+          (message) => message.id === data.id
+        );
+
+        // If the message with the same ID doesn't exist, add it to the list
+        if (!isMessageAlreadyInList) {
+          return [...list, data];
+        }
+
+        // If the message already exists, return the original list without any changes
+        return list;
+      });
     });
   }, [socket]);
 
   return (
     <div className="chat-window">
-      <div className="chat-header">
-        <p>Live Chat</p>
+      <div className="chat-header mb-3">
+        <div className="center-alert">
+          <Alert key="info" variant="info">
+            WELCOME TO THE CHAT
+          </Alert>
+        </div>
       </div>
       <div className="chat-body">
-        <ScrollToBottom className="message-container">
-          {messageList.map((messageContent, index) => {
-            return (
-              <div
-                className="message"
-                id={user === messageContent.author ? "you" : "other"}
-                key={index}
-              >
-                <div>
-                  <div className="message-content">
-                    <p>{messageContent.message}</p>
+        <Container>
+          <Row>
+            <Col>
+              <ScrollToBottom className="message-container">
+                {messageList.map((messageContent, index) => (
+                  <div
+                    className="message mb-3"
+                    id={user === messageContent.author ? "you" : "other"}
+                    key={index}
+                  >
+                    <Card>
+                      <Card.Body>
+                        <Card.Text>{messageContent.message}</Card.Text>
+                      </Card.Body>
+                      <Card.Footer>
+                        <small className="text-muted">
+                          {messageContent.time} by {messageContent.author}
+                        </small>
+                      </Card.Footer>
+                    </Card>
                   </div>
-                  <div className="message-meta">
-                    <p id="time">{messageContent.time}</p>
-                    <p id="author">{messageContent.author}</p>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-        </ScrollToBottom>
+                ))}
+              </ScrollToBottom>
+            </Col>
+          </Row>
+        </Container>
       </div>
       <div className="chat-footer">
-        <input
-          type="text"
-          value={currentMessage}
-          placeholder="Hey..."
-          onChange={(event) => {
-            setCurrentMessage(event.target.value);
-          }}
-          onKeyDown={handleKeyDown} // Use onKeyDown event handler
-        />
-        <button onClick={sendMessage}>&#9658;</button>
+        <Container>
+          <Row>
+            <Col>
+              <Form.Control
+                type="text"
+                value={currentMessage}
+                placeholder="Hey..."
+                onChange={(event) => {
+                  setCurrentMessage(event.target.value);
+                }}
+                onKeyDown={handleKeyDown} // Use onKeyDown event handler
+              />
+            </Col>
+            <Col xs="auto">
+              <Button onClick={sendMessage} variant="primary">
+                &#9658;
+              </Button>
+            </Col>
+          </Row>
+        </Container>
       </div>
     </div>
   );
