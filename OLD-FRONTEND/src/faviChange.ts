385688568845bc436f1f4bb0c0a741f382967ad4
tom <83908974+goat6@users.noreply.github.com>
2023-09-22 14:33:51 +0100

    delete old frontend

--- OLD-FRONTEND/src/faviChange.ts
+++
@@ -1,9 +0,0 @@
-const faviChange = () => {
-  const link = document.querySelector("link[rel*='icon']") as HTMLLinkElement || document.createElement('link');
-  link.type = "image/svg+xml"
-  link.rel = "icon"
-  link.href = "test.svg"
-  document.head.appendChild(link)
-}
-
-export default faviChange
