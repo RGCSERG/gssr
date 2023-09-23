c8e41edecc464823da9c57457f4229e3ecdae4a5
tom <83908974+goat6@users.noreply.github.com>
2023-09-23 13:40:55 +0100

    added functions into hooks

--- client/src/hooks/UseChat.tsx
+++ client/src/hooks/UseChat.tsx
@@ -3,53 +3,57 @@ import {
   getRandomInt,
 } from "../functions/functions";
 import { ChatMessage } from "../interfaces/ChatMessage/ChatMessage";
-import { socket } from "./UseRoom";
+import useMessageList from "./useMessageList";
 
-export const sendChatMessage = (message: ChatMessage) => {
-  if (socket) {
-    socket.emit("chat_message", message);
-  }
-};
+import useSocket from "./useSocket";
 
-export const sendMessage = async (
-  currentMessage: string,
-  room: string,
-  user: string,
-  updateMessageList: (
-    newMessage: ChatMessage,
-    setMessageList: React.Dispatch<React.SetStateAction<ChatMessage[]>>
-  ) => void,
-  setMessageList: React.Dispatch<React.SetStateAction<ChatMessage[]>>
-) => {
-  if (currentMessage.trim() !== "" && room !== undefined) {
-    const messageData: ChatMessage = {
-      id: getRandomInt(1000000000000, 9999999999999),
-      room: room,
-      author: user,
-      message: currentMessage,
-      time: formatTimeWithLeadingZeros(),
-    };
-    updateMessageList(messageData, setMessageList);
-    sendChatMessage(messageData);
-  }
-};
+const useChat = () => {
+  const { socket } = useSocket();
+  const { setMessageList } = useMessageList();
 
-export const updateMessageList = (
-  newMessage: ChatMessage,
-  setMessageList: React.Dispatch<React.SetStateAction<ChatMessage[]>>
-) => {
-  setMessageList((list) => {
-    // Check if the message with the same ID exists in the list
-    const isMessageAlreadyInList = list.some(
-      (message) => message.id === newMessage.id
-    );
-
-    // If the message with the same ID doesn't exist, add it to the list
-    if (!isMessageAlreadyInList) {
-      return [...list, newMessage];
+  const sendChatMessage = (message: ChatMessage) => {
+    if (socket) {
+      socket.emit("chat_message", message);
     }
+  };
 
-    // If the message already exists, return the original list without any changes
-    return list;
-  });
+  const sendMessage = async (
+    currentMessage: string,
+    room: string,
+    user: string
+  ) => {
+    if (currentMessage.trim() !== "" && room !== undefined) {
+      const messageData: ChatMessage = {
+        id: getRandomInt(1000000000000, 9999999999999),
+        room: room,
+        author: user,
+        message: currentMessage,
+        time: formatTimeWithLeadingZeros(),
+      };
+      updateMessageList(messageData);
+      sendChatMessage(messageData);
+    }
+  };
+
+  const updateMessageList = (newMessage: ChatMessage) => {
+    console.log("updating message list!");
+    setMessageList((list) => {
+      // Check if the message with the same ID exists in the list
+      const isMessageAlreadyInList = list!.some(
+        (message) => message.id === newMessage.id
+      );
+
+      // If the message with the same ID doesn't exist, add it to the list
+      if (!isMessageAlreadyInList) {
+        return [...list!, newMessage];
+      }
+
+      // If the message already exists, return the original list without any changes
+      return list;
+    });
+  };
+
+  return { sendMessage, updateMessageList };
 };
+
+export default useChat;
