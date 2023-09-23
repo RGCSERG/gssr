5133477ab6d09a299796dd5daf34d8365506b95f
RGCSERG <sergentrufus@gmail.com>
2023-09-23 15:23:08 +0100

    trying fix

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import MessageContext from "./contexts/MessageListContext";
 import { SocketProvider } from "./contexts/SocketContext";
@@ -8,12 +8,12 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <MessageContext>
         <SocketProvider>
           <App />
         </SocketProvider>
       </MessageContext>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
