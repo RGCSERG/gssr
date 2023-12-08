650e2b0f231c10b82901eb726104a275d2345495
tom <83908974+goat6@users.noreply.github.com>
2023-12-08 11:03:01 +0000

    Added column dropping & errors

--- client/src/components/GameBoard.tsx
+++ client/src/components/GameBoard.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { number } from "zod";
 
 const GameBoard = () => {
   let a: number[] = [];
@@ -9,19 +10,55 @@ const GameBoard = () => {
   }
 
   const [player, setPlayer] = useState<number>(1);
+  const [error, setError] = useState<string>("");
 
   const [array, updateArray] = useState<number[]>(a);
 
+  const handleMouseEnter = () => {};
+
+  const getIndices = (column: number): number[] => {
+    let array: number[] = [];
+    for (let i = 0; i < 9; i++) {
+      const num: number = column + 9 * i;
+      array.push(num);
+    }
+    return array;
+  };
+
+  const getEmptySlot = (indices: number[]): number => {
+    let slot: number = -1;
+    indices.map((value) => {
+      if (array[value] == 0) {
+        slot = value;
+      }
+    });
+    return slot;
+  };
+
   const handleClick = (id: number) => {
-    player === 1 ? setPlayer(2) : setPlayer(1);
-    // Create a new array with the updated value
-    const newArray = array.map((value, index) =>
-      index === id ? (player === 1 ? (value = 1) : (value = 2)) : value
-    );
+    const column: number = id % 9;
+
+    const indices: number[] = getIndices(column);
+    const slot: number = getEmptySlot(indices);
+
+    const newArray = array.map((value, index) => {
+      if (index == slot) {
+        if (value == 0) {
+          player === 1 ? setPlayer(2) : setPlayer(1);
+          // console.log("Found empty slot at index: " + slot);
+          return player;
+        } else {
+          return value;
+        }
+      } else {
+        if (slot == -1) {
+          setError("Please choose a non full column");
+        }
+        return value;
+      }
+    });
     // Update the state with the new array
     updateArray(newArray);
-
-    console.log(`Altered tile ${id}`);
   };
 
   // useEffect(() => {}, [array]);
@@ -29,13 +66,18 @@ const GameBoard = () => {
   return (
     <div>
       <h1 className="text-4xl p-4">Connect 4: {player}'s turn</h1>
+      {error != "" ? (
+        <h1 className="text-4xl text-red-500 p-4">{error}</h1>
+      ) : null}
       <div className="grid grid-cols-9 p-10">
         {array.map((value, index) => (
           <div
+            onMouseEnter={}
             key={index}
             className={`border border-black-500 h-16
             ${value == 1 ? "bg-blue-500" : null}
             ${value == 2 ? "bg-red-500" : null}
+            hover:bg-slate-200 hover:bg-opacity-50
           `}
             onClick={() => handleClick(index)}
           >
